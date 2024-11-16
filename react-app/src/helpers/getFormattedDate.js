64b340edff557f42c4a731e736e0eb016f8fa18e
misyuk <misyuktaras@gmail.com>
2024-11-16 18:47:40 +0200

    Update jira submit to start tracking at the middle of the day

--- react-app/src/helpers/getFormattedDate.js
+++ react-app/src/helpers/getFormattedDate.js
@@ -17,5 +17,5 @@ export const getCorrectGMTDateObject = (dateString) => {
 
 export const formatDateForJira = (date) => {
   const [day, month, year] = date.split("-");
-  return `${year}-${month}-${day}T00:00:00.000+0000`;
+  return `${year}-${month}-${day}T12:00:00.000+0000`;
 };
